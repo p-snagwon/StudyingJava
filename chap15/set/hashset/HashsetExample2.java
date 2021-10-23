8955d956792188d8279053cadd40e241b5b5c06f
p-snagwon <63057144+p-snagwon@users.noreply.github.com>
2021-10-23 17:16:08 +0900

    Add files via upload

---
+++ chap15/set/hashset/HashsetExample2.java
@@ -0,0 +1,18 @@
+package chap15.set.hashset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashsetExample2 {
+
+    public static void main(String[] args) {
+
+        Set<Member> set = new HashSet<Member>();
+
+        set.add(new Member("홍길동", 30));
+        set.add(new Member("홍길동", 30));
+
+        System.out.println("총객체 수: " + set.size());
+
+    }
+}
---
+++ chap15/set/hashset/Member.java
@@ -0,0 +1,28 @@
+package chap15.set.hashset;
+
+import java.util.Objects;
+
+public class Member {
+    public String name;
+    public int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return age == member.age && Objects.equals(name, member.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
