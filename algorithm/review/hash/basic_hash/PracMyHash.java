5a457763bb386a5fe0467ee36298c53eda5e2cb8
p-snagwon <63057144+p-snagwon@users.noreply.github.com>
2021-11-06 22:07:38 +0900

    Add files via upload

--- algorithm/review/hash/basic_hash/PracMyHash.java
+++ algorithm/review/hash/basic_hash/PracMyHash.java
@@ -8,10 +8,14 @@ public class PracMyHash {
     }
 
     public class Slot {
-        public String value;
+        String value;
+        String key;
+        Slot next;
 
-        public Slot(String value) {
+        public Slot(String key,String value) {
+            this.key = key;
             this.value = value;
+            this.next = null;
         }
     }
 
@@ -20,24 +24,42 @@ public class PracMyHash {
     }
 
     public boolean addData(String key, String value) {
-        Integer address = hashFunc(key);
-        if (this.hashTable[address] != null) {
-            this.hashTable[address].value = value;
+        Integer address = this.hashFunc(key);
+        if (hashTable[address] != null) {
+            Slot findSlot = this.hashTable[address];
+            Slot prevSlot = this.hashTable[address];
+            while (findSlot != null) {
+                if (findSlot.key == key) {
+                    findSlot.value = value;
+                    return true;
+                } else {
+                    prevSlot = findSlot;
+                    findSlot = findSlot.next;
+                }
+            }
+            prevSlot.next = new Slot(key, value);
         } else {
-            this.hashTable[address] = new Slot(value);
+            this.hashTable[address] = new Slot(key, value);
         }
         return true;
-    }
+            }
 
 
 
     public String getData(String key) {
-        Integer address = hashFunc(key);
+        Integer address = this.hashFunc(key);
         if (this.hashTable[address] != null) {
-            return this.hashTable[address].value;
-        } else {
+            Slot findSlot = this.hashTable[address];
+            while (findSlot != null) {
+                if (findSlot.key == key) {
+                    return findSlot.value;
+                } else {
+                    findSlot = findSlot.next;
+                }
+            }
             return null;
         }
+        return null;
     }
 
     public static void main(String[] args) {
