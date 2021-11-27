5d2f23ae89cc0efe5fa84798b511b4f94a437f48
p-snagwon <63057144+p-snagwon@users.noreply.github.com>
2021-11-27 22:35:03 +0900

    Add files via upload

--- algorithm/heap/Heap.java
+++ algorithm/heap/Heap.java
@@ -3,25 +3,29 @@ package heap;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 
-public class Heap<E>{
-
-    private final Comparator<? super E> comparator;
-    private static final int DEFAULT_CAPACITY = 10;
+public class Heap<E> {
 
 
-    private int size; // 요소 개수
+    private final Comparator<? super  E> comparator;
+    private static final int DEFUALT_CAPACITY = 10;
 
     private Object[] array;
 
-    //생성자 Type1
+    private int size;
+
+
+    // 용적의 크기가 디폴트일때 1. comparator 가 제공되는 경우 2. 제공되지 않는 경우
+    // 용적의 크기가 제공되는 경우 1. comparator가 제공되는 경우 2. 제공되지않는 경우
+
     public Heap() {
         this(null);
     }
 
     public Heap(Comparator<? super E> comparator) {
-        this.array = new Object[DEFAULT_CAPACITY];
-        this.size = 0;
+
         this.comparator = comparator;
+        this.size = size;
+        this.array = new Object[DEFUALT_CAPACITY];
     }
 
     public Heap(int capacity) {
@@ -29,23 +33,23 @@ public class Heap<E>{
     }
 
     public Heap(int capacity, Comparator<? super E> comparator) {
-        this.array = new Object[capacity];
         this.size = 0;
         this.comparator = comparator;
+        this.array = new Object[capacity];
+
     }
 
-    private int getParent(int index) {
-        return index /2;
+    private int getParent(int idx) {
+        return idx / 2;
     }
 
-    private int getLeftChild(int index) {
-        return index * 2;
+    private int getLeftChild(int idx) {
+        return idx * 2;
     }
 
-    private int getRightChild(int index) {
-        return index * 2 + 1;
+    private int getRightChild(int idx) {
+        return (idx * 2) + 1;
     }
-
 
     private void resize(int newCapacity) {
 
@@ -60,59 +64,65 @@ public class Heap<E>{
     }
 
     public void add(E value) {
-        if (size == array.length + 1) {
+
+        if (size + 1 == array.length) {
             resize(array.length * 2);
         }
 
         siftUp(size + 1, value);
         size++;
+
     }
 
+    //추가할 노드의 idx, target 재배치할 노드
     private void siftUp(int idx, E target) {
+
         if (comparator != null) {
             siftUpComparator(idx, target, comparator);
         } else {
             siftUpComparable(idx, target);
         }
+
+    }
+
+    private void siftUpComparable(int idx, E target) {
+
+        Comparable<? super E> comparable = (Comparable<? super E>) target;
+
+        while (idx > 1) {
+
+            int parent = getParent(idx);
+
+            Object parentVal = array[idx];
+
+            if (comparable.compareTo((E) parentVal) >= 0) {
+                break;
+            }
+
+            array[idx] = parentVal;
+            idx = parent;
+        }
+
+        array[idx] = comparable;
+
     }
 
     private void siftUpComparator(int idx, E target, Comparator<? super E> comp) {
 
         while (idx > 1) {
             int parent = getParent(idx);
+
             Object parentVal = array[parent];
 
-            //타겟 노드 값이 부모노드보다 크면 종료
             if (comp.compare(target, (E) parentVal) >= 0) {
                 break;
             }
 
             array[idx] = parentVal;
             idx = parent;
-
         }
 
         array[idx] = target;
-    }
-
-    private void siftUpComparable(int idx, E target) {
-        //타겟 노드가 비교할 수 있도록 한 변수를 만든다.
-
-        Comparable<? super E> comp = (Comparable<? super E>) target;
-
-        while (idx > 1) {
-            int parent = getParent(idx);
-            Object parentVal = array[parent];
-
-            if (comp.compareTo((E) parentVal) >= 0) {
-                break;
-            }
-
-            array[idx] = parentVal;
-            idx = parent;
-        }
-
-        array[idx] = comp;
     }
 
     public E remove() {
@@ -133,50 +143,11 @@ public class Heap<E>{
         if (comparator != null) {
             siftDownComparator(idx, target, comparator);
         } else {
-            siftDownComparator(idx, target);
+            siftDownComparable(idx, target);
         }
     }
 
-    private void siftDownComparator(int idx, E target, Comparator<? super E> comp) {
-        array[idx] = null;
-        size--;
-
-        int parent = idx;
-        int child; //교환될 자식을 가리키는 변수
-
-        while ((child = getLeftChild(parent))<= size) {
-
-            int right = getRightChild(parent);
-
-            Object childVal = array[child]; //왼쪽 값
-
-            if (right <= size && comp.compare((E) childVal, (E) array[right]) > 0) {
-                child = right;
-                childVal = array[child];
-            }
-
-            if (comp.compare(target, (E) childVal) <= 0) {
-                break;;
-            }
-
-            array[parent] = childVal;
-            parent = child;
-
-        }
-
-        array[parent] = target;
-
-
-        if (array.length > DEFAULT_CAPACITY && size < array.length / 4) {
-            resize(Math.max(DEFAULT_CAPACITY, array.length /2));
-        }
-
-
-    }
-
-    private void siftDownComparable(int idx, E target) {
-        Comparable<? super E> comp = (Comparable<? super E>) target;
-
+    private void siftDownComparator(int idx, E target, Comparator<? super E> comparator) {
         array[idx] = null;
         size--;
 
@@ -188,24 +159,21 @@ public class Heap<E>{
 
             Object childVal = array[child];
 
-            if(right <= size && ((Comparable<? super E>)childVal).compareTo((E)array[right]) > 0) {
+            if (right <= size && comparator.compare((E) childVal, (E) array[right]) > 0) {
                 child = right;
                 childVal = array[child];
             }
 
-            if (comp.compareTo((E) childVal) <= 0) {
+            //target이 크면 양수
+            if (comparator.compare(target, (E) childVal) <= 0) {
+
                 break;
             }
 
             array[parent] = childVal;
             parent = child;
         }
-        array[parent] = comp;
-
-
-        if(array.length > DEFAULT_CAPACITY && size < array.length / 4) {
-            resize(Math.max(DEFAULT_CAPACITY, array.length / 2));
-        }
-
     }
+
+
 }
